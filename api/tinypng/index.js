3e9aafe8d107f33dea97d34099a5bed80a950476
lvwenhao <lvwenhao@huohua.cn>
2021-12-30 13:34:00 +0800

    测试API

--- api/tinypng/index.js
+++ api/tinypng/index.js
@@ -1,15 +1,18 @@
 const axios = require('axios')
 
 module.exports = async (req, res) => {
-  const image = req.body;
   const result = await axios({
-    method: 'post',
+    method: 'POST',
     url: `https://api.tinify.com/shrink`,
     headers:{
       'authorization':'Basic YXBpOlRQUmg0RlpRWkhQTmpOUW5WTlhYWjNjSnh5eWJGVGgy',
       'Content-type':'application/json'
     },
-    body:image
+    body:JSON.stringify({
+        "source": {
+          "url": "https://tinypng.com/images/panda-happy.png"
+        }
+    })
   })
   res.json({
     status: 'ok',

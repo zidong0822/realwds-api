965f4a00f540107d98c6ff6ac9778ab3758308db
lvwenhao <lvwenhao@huohua.cn>
2021-12-30 12:38:11 +0800

    test bing apu

--- api/tinypng/index.js
+++ api/tinypng/index.js
@@ -1,11 +1,14 @@
+const axios = require('axios')
+
 module.exports = async (req, res) => {
-    const result = await axios({
-      method: 'get',
-      url: `http://cn.bing.com/HPImageArchive.aspx?format=js&idx=${1}&n=${1}`
-    })
   
-    res.json({
-      status: 'ok',
-      data: result.data
-    })
-  }
+  const result = await axios({
+    method: 'get',
+    url: `https://cn.bing.com/HPImageArchive.aspx?format=js&idx=1&n=1`
+  })
+
+  res.json({
+    status: 'ok',
+    data: result.data
+  })
+}

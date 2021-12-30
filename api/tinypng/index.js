bbdb4f68b1a24e9e4fd3ed6b0f09539c28d60eba
lvwenhao <lvwenhao@huohua.cn>
2021-12-30 12:50:15 +0800

    test api tinypng

--- api/tinypng/index.js
+++ api/tinypng/index.js
@@ -1,14 +1,19 @@
 const axios = require('axios')
 
 module.exports = async (req, res) => {
-  
+  const image = req.data;
   const result = await axios({
     method: 'post',
-    url: 'https://api.tinify.com/shrink'
+    url: `https://api.tinify.com/shrink`,
+    headers:{
+      'authorization':'Basic YXBpOlRQUmg0RlpRWkhQTmpOUW5WTlhYWjNjSnh5eWJGVGgy',
+      'Content-type':'application/json'
+    },
+    body:image
   })
-  console.log('测试结果',result);
   res.json({
     status: 'ok',
     data: result.data
   })
 }
+

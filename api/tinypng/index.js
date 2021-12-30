c732885ca9232314c0ff39cf70b951dcebdc6520
lvwenhao <lvwenhao@huohua.cn>
2021-12-30 14:12:19 +0800

    允许跨域

--- api/tinypng/index.js
+++ api/tinypng/index.js
@@ -1,22 +1,39 @@
 const axios = require('axios')
-
-module.exports = async (req, res) => {
-  const result = await axios({
-    method: 'POST',
-    url: `https://api.tinify.com/shrink`,
-    headers:{
-      'authorization':'Basic YXBpOlRQUmg0RlpRWkhQTmpOUW5WTlhYWjNjSnh5eWJGVGgy',
-      'Content-type':'application/json'
-    },
-    data:JSON.stringify({
-        "source": {
-            "url": "https://tinypng.com/images/panda-happy.png"
-        }
-      })
-  })
-  res.json({
-    status: 'ok',
-    data: result.data
-  })
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
 }
 
+const getTinyPng = async (req, res) => {
+    const result = await axios({
+      method: 'POST',
+      url: `https://api.tinify.com/shrink`,
+      headers:{
+        'authorization':'Basic YXBpOlRQUmg0RlpRWkhQTmpOUW5WTlhYWjNjSnh5eWJGVGgy',
+        'Content-type':'application/json'
+      },
+      data:JSON.stringify({
+          "source": {
+              "url": "https://tinypng.com/images/panda-happy.png"
+          }
+        })
+    })
+    res.json({
+      status: 'ok',
+      data: result.data
+    })
+  }
+
+module.exports = allowCors(getTinyPng);

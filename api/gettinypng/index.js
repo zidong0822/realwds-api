5ca85fac98a975b0dfd9cb7bc97d71df27db061c
lvwenhao <lvwenhao@huohua.cn>
2021-12-31 11:05:35 +0800

    获取图片接口

---
+++ api/gettinypng/index.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+}
+const getTinyPng = async (req, res) => {
+    const url = req.query.url;
+    const result = await axios({
+      url: url,
+      headers:{
+        'authorization':'Basic YXBpOlRQUmg0RlpRWkhQTmpOUW5WTlhYWjNjSnh5eWJGVGgy'
+      }
+    })
+  res.json(result.data);
+}
+
+module.exports = allowCors(getTinyPng);
